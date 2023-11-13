26731da41101e5ed31f38e67659b12cacbb00953
dias-ko <56513270+dias-ko@users.noreply.github.com>
2023-11-13 17:11:35 +0600

    Code clean-up and optimization

--- Magnet/main.py
+++ Magnet/main.py
@@ -1,102 +1,51 @@
-
-### --- same as before ---
-
-### ---------------------- 
-  
 from umqtt.simple import MQTTClient
-import time
-import esp32
-import ubinascii
-import machine
-import micropython
-import network
+from time import sleep
+from esp32 import wake_on_ext0, WAKEUP_ANY_HIGH, WAKEUP_ALL_LOW
+from machine import Pin, RTC, deepsleep
+from network import WLAN, STA_IF
 
-
-sensor = machine.Pin(4, mode = machine.Pin.IN)
-
-
-door_topic = b'door'
-CLIENT_NAME = 'DOOR1'
+sensor = Pin(4, mode = Pin.IN)
 
 def change_signal(status):
     if status == 1:
-        print("Status open")
-        esp32.wake_on_ext0(pin = sensor, level = esp32.WAKEUP_ANY_HIGH)
+        # print("Wake up on door closure")
+        wake_on_ext0(pin = sensor, level = WAKEUP_ANY_HIGH)
     else:
-        print("Status closed")
-        esp32.wake_on_ext0(pin = sensor, level = esp32.WAKEUP_ALL_LOW)
+        # print("Wake up on door opening")
+        wake_on_ext0(pin = sensor, level = WAKEUP_ALL_LOW)
 
-# 
-# def wait_pin_change(pin):
-#     # wait for pin to change value
-#     # it needs to be stable for a continuous 20ms
-#     cur_value = pin.value()
-#     active = 0
-#     while active < 20:
-#         if pin.value() != cur_value:
-#             active += 1
-#         else:
-#             active = 0
-#         time.sleep(0.001)
-
-# def connect_mqtt():
-# station = network.WLAN(network.STA_IF)
-# ssid = "NU"
-# password = "1234512345"
-# station.active(True)
-# station.connect(ssid, password)
-# 
-# while station.isconnected() == False:
-#   pass
-# 
-# 
-# print('Connection successful')
-# BROKER_ADDR = 'test.mosquitto.org'
-# mqttc = MQTTClient(CLIENT_NAME, BROKER_ADDR, port = 1883, keepalive=60)
-# mqttc.connect()
-
-
-def get_door():
+def isOpen():
+    # 1 - door is open and 0 - door is closed
     return 1 - sensor.value()
-#   print("Door is closed")
-        
-    
-#   print("Door is open")
-     # 1 is open, magnet and 0 is closed
 
-rtc = machine.RTC()
-prev_stat = rtc.memory()
-if prev_stat == b'':
-    prev_stat = 1-get_door()
-    print(prev_stat)
-    change_signal(prev_stat)
-elif prev_stat == b'0':
-    prev_stat = 0
-elif prev_stat == b'1':
-    prev_stat = 1
-else:
-    print("wtf")
-    sleep(1000)
-    
-    
+rtc = RTC()
+prev_status = rtc.memory()
 
-# wait_pin_change(sensor)
+if prev_status == b'':
+    curr_status = isOpen()
+    rtc.memory(b'' + str(curr_status))
+    change_signal(curr_status)
+    deepsleep()
+elif prev_status == b'0':
+    prev_status = 0
+elif prev_status == b'1':
+    prev_status = 1
+
+# wait pin change(sensor)
 active = 0
 while active < 20 and active > -20:
-    if prev_stat != get_door():
+    if prev_status != isOpen():
         active += 1
     else:
         active -= 1
-    time.sleep(0.05)
+    sleep(0.05)
     
 if active > 0:
-    curr_stat = 1 - prev_stat
-    change_signal(curr_stat)
-    rtc.memory(b'' + str(curr_stat))
-#     print("I am here")
-#     mqttc.publish(door_topic, "0")
-#     print("I got here")
-    station = network.WLAN(network.STA_IF)
+    curr_status = 1 - prev_status
+    change_signal(curr_status)
+    rtc.memory(b'' + str(curr_status))
+
+    station = WLAN(STA_IF)
     ssid = "NU"
     password = "1234512345"
     station.active(True)
@@ -105,17 +54,19 @@ if active > 0:
     while station.isconnected() == False:
       pass
 
-
     print('Connection successful')
     BROKER_ADDR = 'test.mosquitto.org'
+    CLIENT_NAME = 'DOOR1'
+    TOPIC       = b'door'
     mqttc = MQTTClient(CLIENT_NAME, BROKER_ADDR, port = 1883, keepalive=60)
     mqttc.connect()
+    mqttc.publish(TOPIC,  b'' + str(curr_status))
 
-    mqttc.publish(door_topic,  b'' + str(curr_stat))
 else:
-    change_signal(prev_stat)
-    rtc.memory(b'' + str(prev_stat))
+    change_signal(prev_status)
+    rtc.memory(b'' + str(prev_status))
+
 print("Going to sleep")
-time.sleep(3)
+sleep(3)
 print("Going to deepsleep")
-machine.deepsleep()
+deepsleep()
--- Proximity/main.py
+++ Proximity/main.py
@@ -1,19 +1,13 @@
 from umqtt.simple import MQTTClient
-import time
-import esp32
-import ubinascii
-import machine
-import micropython
-import network
+from time import sleep
+from machine import RTC, deepsleep
+from network import WLAN, STA_IF
 from hcsr04 import HCSR04
 
-sensor = HCSR04(trigger_pin=5, echo_pin=4, echo_timeout_us=10000)
-
-door_topic = b'door'
-CLIENT_NAME = 'DOOR1'
+sensor = HCSR04(trigger_pin = 5, echo_pin = 4, echo_timeout_us = 10000)
 
 def isOpen():
-    # 1 is open, magnet and 0 is closed
+    # 1 - door is open and 0 - door is closed
     distance = sensor.distance_cm()
     if distance > 3.:
         print("door is open")
@@ -21,25 +15,19 @@ def isOpen():
         print("door is closed")
     return 1 if distance > 3. else 0
 
-rtc = machine.RTC()
+rtc = RTC()
 prev_stat = rtc.memory()
 
-# if prev_stat == b'':
-#     prev_stat = 1 - isOpen()
-# el
 if prev_stat == b'0':
     prev_stat = 0
 elif prev_stat == b'1':
     prev_stat = 1
-else:
-    print("wtf")
-    sleep(1000)
 
 curr_stat = isOpen()
 rtc.memory(b'' + str(curr_stat))
 
 if curr_stat != prev_stat:
-    station = network.WLAN(network.STA_IF)
+    station = WLAN(STA_IF)
     ssid = "NU"
     password = "1234512345"
     station.active(True)
@@ -49,12 +37,17 @@ if curr_stat != prev_stat:
       pass
 
     print('Connection successful')
+    
+    CLIENT_NAME = 'DOOR1'
     BROKER_ADDR = 'test.mosquitto.org'
-    mqttc = MQTTClient(CLIENT_NAME, BROKER_ADDR, port = 1883, keepalive=60)
+    PORT        = 1883
+    TOPIC       = b'door'
+
+    mqttc = MQTTClient(CLIENT_NAME, BROKER_ADDR, PORT, keepalive = 60)
     mqttc.connect()
-    mqttc.publish(door_topic,  b'' + str(curr_stat))
+    mqttc.publish(TOPIC,  b'' + str(curr_stat))
 
 print("Going to sleep")
-time.sleep(3)
+sleep(3)
 print("Going to deepsleep")
-machine.deepsleep(5)
+deepsleep(5)
