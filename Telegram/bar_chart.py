30840a2a75afd19702fbf79c3589b3dea3514bf6
dias-ko <56513270+dias-ko@users.noreply.github.com>
2023-11-16 01:50:58 +0600

    Telegram bot final version

---
+++ Telegram/bar_chart.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import numpy as np
+from datetime import datetime, timedelta
+import random
+
+def calculate_weights(timestamps):
+    weights = []
+    now = datetime.now()
+    s = len(timestamps)
+    for i in range(s):
+        if i == 0:
+            w = (timestamps[i+1] - (now - timedelta(days=1))).seconds / 3600
+        elif i == s-1:
+            w = (now - timestamps[i]).seconds / 3600
+        else:
+            w = (timestamps[i+1] - timestamps[i]).seconds / 3600
+        weights.append(w)
+    return weights
+    
+def calculate_starts(timestamps):
+    starts = [0]
+    now = datetime.now()
+    for i in range(1, len(timestamps)):
+        if i == 1:
+            s = (timestamps[i] - (now - timedelta(days=1))).seconds / 3600
+        else:
+            s = starts[i-1] + (timestamps[i] - timestamps[i-1]).seconds / 3600
+        starts.append(s)
+    return starts
+
+def generate_timestamps(n):
+    current_time = datetime.now()
+    timestamps = [current_time - timedelta(hours=random.randint(1, 23), minutes=random.randint(0, 59)) for _ in range(n-1)]
+    timestamps.append(current_time - timedelta(days=1, minutes=59))
+    timestamps.sort()
+    door_status = np.tile([0,1],24//2)
+    return timestamps, door_status
+
+def get_plot(weights, starts, colors):
+    plt.figure(figsize=(7, 2.43))
+    plt.barh(0, weights, color=colors, height=1, left=starts)
+
+    # Customize the plot
+    plt.yticks([])
+    plt.xticks(np.arange(7.5, 31.5, 8), [(datetime.now() - timedelta(hours=i)).strftime('%H:%M') for i in range(0, 24, 8)][::-1])
+    plt.rcParams['font.sans-serif'] = "Roboto"
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['bottom'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.tick_params(axis="x", bottom=False)
+    plt.tight_layout()
+    plt.margins(x=0, y=0)
+    legend_elements = [Patch(facecolor='#F84F31', label='Closed'),
+                       Patch(facecolor='#23C552', label='Open')]
+    plt.legend(handles=legend_elements, loc='lower left', bbox_to_anchor=(-0.01, -0.17), prop={'size': 9}, ncols=2)
+    
+    # Save the plot
+    filename = f'charts/door_status {datetime.now().strftime("%Y-%m-%d %H-%M-%S-%f")}.png'
+    plt.savefig(filename)
+    # plt.show()
+    return filename
+
+def get_daily_status(data):
+    data = np.array(data)
+    t1, t2 = np.split(data, 2, axis = 1)
+    timestamps = t1.flatten().tolist()
+    door_status = t2.flatten().tolist()
+    # timestamps, door_status = generate_timestamps(24)
+    weights = calculate_weights(timestamps)
+    starts = calculate_starts(timestamps)
+    colors = ['#F84F31' if status == 0 else '#23C552' for status in door_status]
+    return get_plot(weights, starts, colors)
--- Telegram/mqtt_telegram.py
+++ Telegram/mqtt_telegram.py
@@ -1,74 +1,147 @@
-import asyncio
-from telegram.ext import Application
 import paho.mqtt.client as mqtt
-from threading import Thread
+import asyncio
+import requests
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from bar_chart import get_daily_status
+import csv
 
-#for telegram
-TELEGRAM_TOKEN = '6780969852:AAFVgqyfmj8v4bw911R-AYem5-sRD0rcekI'
-CHAT_ID = '-1002064110322'
+load_dotenv()
 
-#for mqtt
-MQTT_BROKER = 'test.mosquitto.org'
-MQTT_PORT = 1883
-MQTT_TOPIC = 'door/status'
+# Telegram bot details
+TELEGRAM_TOKEN  = os.environ.get('TELEGRAM_TOKEN')
+CHAT_ID         = os.environ.get('CHAT_ID')
+
+# MQTT Connection details
+MQTT_BROKER = os.environ.get('MQTT_BROKER')
+MQTT_PORT   = int(os.environ.get('MQTT_PORT'))
+MQTT_TOPIC  = os.environ.get('MQTT_TOPIC')
+
+DATA_FILE = 'data.csv'
+
+class DoorStatues:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = self.read_csv()
+        self.daily_list = self.process_data()
+
+    def read_csv(self):
+        data = []
+        try:
+            with open(self.file_path, 'r') as file:
+                csvReader = csv.reader(file)
+                for row in csvReader:
+                    data.append([datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S.%f'), int(row[1])])
+                return data
+        except FileNotFoundError:
+            print("File not found.")
+            return None
+        
+    def filter_last_24_hours(self, data):
+        filtered_data = []
+        current_time = datetime.now()
+        for timestamp, status in reversed(data):
+            filtered_data.append([timestamp, status])
+            if current_time - timestamp > timedelta(days=1):
+                break
+        return filtered_data
+
+    def process_data(self):
+        filtered_data = self.filter_last_24_hours(self.data)[::-1]
+        return filtered_data
+
+    def add_field(self, timestamp, status):
+        with open(self.file_path, 'a', newline='') as csvfile:
+            csvWriter = csv.writer(csvfile)
+            csvWriter.writerow([timestamp, status])
+        self.daily_list.append((timestamp, status))
+        return
+
+    def get_last_24_hours(self):
+        return self.daily_list
+
+def decoder(dct):
+    if 'Timestamp' and 'Status' in dct:
+        return datetime.strptime(dct['Timestamp'],'%Y-%m-%d %H:%M:%S.%f'), int(dct['Status'])
+    return dct
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print(f"Failed to connect, return code {rc}")
     client.subscribe(MQTT_TOPIC)
 
-def on_mqtt_message(client, userdata, msg, bot):
-    asyncio.run(send_message(client,userdata,msg,bot))
+def on_message(client, userdata, msg):
+    asyncio.run(send_message(client,userdata,msg))
 
-async def send_message(client, userdata,msg,bot):
+async def send_message(client, userdata, msg):
     message = int(msg.payload)
-    print("The message is ")
-    print(message)
+    now = datetime.now()
+    door_data.add_field(now, message)
     if message == 1:
-        text = '🚪Door is open🟩'
+        text = f"\U0001F7E9 Door is <b>OPEN</b>\n\U0001F551 Time: {now.strftime('%H:%M:%S')}"
     else:
-        text = '🚪Door is closed🟥'
-    await bot.send_message(chat_id=CHAT_ID, text=text)
-    #, asyncio.get_event_loop()
-    print("Test message sent.")
+        text = f"\U0001F7E5 Door is <b>CLOSED</b>\n\U0001F551 Time: {now.strftime('%H:%M:%S')}"
+    data = {"chat_id": CHAT_ID, "text": text, "parse_mode":"HTML"}
+    res = requests.get(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage", data=data)
+    code = res.status_code
+    if code == 200:
+        print("Message send successfully")
+    else:
+        print(f"Message not delivered, response code {code}")
 
 def run_mqtt():
     mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_mqtt_message
+    mqtt_client.on_message = lambda client, userdata, message: on_message(client, userdata, message)
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    mqtt_client.loop_forever()
-
-# async def send_message(bot):
-#     try:
-#         await bot.send_message(chat_id=CHAT_ID, text="🚪Door is open🟩 - testing works without error")
-#         print("Test message sent.")
-#     except Exception as e:
-#         print(f"Error sending test message: {e}")
-
-# def start_async_tasks(bot):
-#     asyncio.run(send_message(bot))
-
-def run_mqtt(bot):
-    mqtt_client = mqtt.Client()
-    mqtt_client.on_connect = on_connect
-    print("Connected on_connect")
-    mqtt_client.on_message = lambda client, userdata, message: on_mqtt_message(client, userdata, message, bot)
-    print("Connected message")
-    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60) 
-    print("Connected ")
-    mqtt_client.loop_forever() #idk
-
-# def start_bot(bot):
-#     bot.run_polling()
+    mqtt_client.loop_start()
 
 def run_telegram_bot():
-    application = Application.builder().token(TELEGRAM_TOKEN).build()
-    bot = application.bot
-    mqtt_thread = Thread(target=run_mqtt, args=(bot,))
-    mqtt_thread.start()
+    run_mqtt()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(periodic_post(door_data))
+    loop.run_forever()
+    
+async def post_chart(door_data):
+    chart_data = door_data.get_last_24_hours()
+    if chart_data is None:
+        print("No data")
+        return
+    lastOpen = "more than a day ago"
+    lastClose = "more than a day ago"  
+    for timestamp, status in chart_data:
+        if status == 1:
+            lastOpen = timestamp
+        else:
+            lastClose = timestamp
+    if lastOpen is not None:
+        lastOpen = lastOpen.strftime("%H:%M")
+    if lastClose is not None:
+        lastClose = lastClose.strftime("%H:%M")
 
-    mqtt_thread.join()
-    application.run_polling()
+    image_path = get_daily_status(chart_data)
+    text = f"Door Status for the last 24 hours\n<i>\U0001F7E9 Last Opened: {lastOpen}\n\U0001F7E5 Last Closed: {lastClose}</i>"
+    data = {"chat_id": CHAT_ID, "caption": text, "parse_mode":"HTML"}
+    with open(image_path, "rb") as image_file:
+        res = requests.post(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendPhoto", data=data, files={"photo": image_file})
+    code = res.status_code
+    if code == 200:
+        print("Chart send successfully")
+    else:
+        print(f"Chart not delivered, response code {code}")
+
+async def wait_periodically(door_data):
+    while True:
+        await post_chart(door_data)
+        await asyncio.sleep(3600)
+
+async def periodic_post(door_data):
+    await wait_periodically(door_data)
 
 if __name__ == '__main__':
+    door_data = DoorStatues(DATA_FILE)
     run_telegram_bot()
